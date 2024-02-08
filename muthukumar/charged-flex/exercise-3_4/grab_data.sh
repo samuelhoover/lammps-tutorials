13f9d3a69cf233f0c426440bb13c6d573ca7bea9
Samuel Hoover <hooversamuelc@gmail.com>
2024-02-08 13:30:21 -0500

    Removed unnecessary scripts

--- muthukumar/charged-flex/exercise-3_4/grab_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DATAPATH=/home/samhoover/projects/lammps-tutorial/tutorial-1/neutral-flex/exercise-2_4-NO-RECENTER
-
-rsync -avzP samhoover@172.30.64.26:$DATAPATH/msd-files/msd*.txt ./msd_files/
-rsync -avzP samhoover@172.30.64.26:$DATAPATH/figures/*.png ./figures/
